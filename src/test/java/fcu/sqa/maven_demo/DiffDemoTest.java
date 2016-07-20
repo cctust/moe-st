e95b3fa0e7b84f12b9301105918f82dba518e0a7
cc <cc@tust.edu.tw>
2016-07-20 15:51:25 +0800

    ok

---
+++ src/test/java/fcu/sqa/maven_demo/DiffDemoTest.java
@@ -0,0 +1,16 @@
+package fcu.sqa.maven_demo;
+
+public class DiffDemoTest {
+String regular, triangle, rightAngle, notTriangle;	
+	
+
+	public void setup() {
+		regular = "regular triangle";
+		triangle = "triangle";
+		rightAngle = "right-angled triangle";
+		notTriangle = "not a triangle";
+	}
+
+
+
+}
